924771e54ccc47e6295a9dfc3e288681e5c94327
Anthony Marc Symkowick <symk1456@mlc104.csumb.edu>
2015-09-17 11:24:34 -0700

    Added folder with Player Images and connected css to index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+	
+
+/*	
+ *  Each player gets from four to six random cards.
+ * 	Each card must be different (no duplicated values).
+ *  Jack is 11 points, Queen 12, and King 13. Ace is always one.
+ * 	The player with the sum of card values lower and closer to 42 wins.
+ *  Values greater than 42 lose the game automatically.
+ * 
+ */
+ 
+
+
+
+?>
+
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<link href="styles.css" rel = "stylesheet">
+	</head>
+</html>
