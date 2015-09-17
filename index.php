8502f901282220411f96ae8a1fb0dfb7dac13338
Uriel Munoz Antonio <anto1513@mlc104.csumb.edu>
2015-09-17 11:44:36 -0700

    get hand fixed

--- index.php
+++ index.php
@@ -1,30 +1,56 @@
 <?php
 
- function getHand()
- {
- 		$deck = array();
+$deck = array();
 
 	for ($i = 0; $i < 52; $i++) {
 		$deck[] = $i;
 	}
 	shuffle($deck);
-	//print_r($deck);
+	
+	$points = array(0 , 0 , 0 ,0);
+	
+function doAll()
+{
+	
+	
+	for($i=0; $i<4; $i++)
+	{
+		getPlayer($i);
+		
+		getHand($i);
+		
+	}
+}
+function getPlayer($i)
+{
+	
+	
+}
 
+
+function getHand($player) {
+	
+	//print_r($deck);
+ 	global $deck;
+	global $points;
 	$amount = rand(4, 6);
 
 	//echo $amount;
-	
+
 	for ($i = 0; $i < $amount; $i++) {
 
 		$lastCard = array_pop($deck);
 
 		$suitArray = array("clubs", "diamonds", "hearts", "spades");
-
-		echo "<img src= 'Silverjack/cards/" . $suitArray[floor($lastCard / 13)] . "/" . (($lastCard % 13) + 1) . ".png'/>";
- }
-
-
-	
+		
+		$number = (($lastCard % 13) + 1);
+		$points[$player] += $number;
+		echo "<img src= 'Silverjack/cards/" . $suitArray[floor($lastCard / 13)] . "/" . $number . ".png'/>";
+		
+		
+	}
+	echo $points[$player] ;
+	echo "<br/> <br/>";
 
 }
 ?>
